14f30bd030af28eb6e2e8894b8ec9d54206d0cbd
Freddy Persson <freddy.persson@24hr.se>
2021-09-23 18:14:36 +0200

    Added a way to handle general settings for different languages

--- lib/content/upsert.php
+++ lib/content/upsert.php
@@ -10,6 +10,25 @@ trait UpsertTrait {
         $permalink = $this->replace_hosts($permalink);
         // remove trailing slash, but keep single slash which is the start page permalink
         $permalink = preg_replace('/(.)\/$/', '$1', $permalink);
+
+        // globals
+		global $sitepress;
+        global $post_type;
+
+        $langSuffix = '';
+
+        if ($post_type != 'page' && isset($sitepress)) {
+            $current_language = $sitepress->get_current_language();
+            $default_language = $sitepress->get_default_language();
+
+            if ($current_language != $default_language) {
+                $langSuffix = '-' . $current_language;
+                $permalink = '/' . $current_language . $permalink;
+            }
+        }
+
+        // error_log('---------------' . $permalinkl);
+
         $content = $this->get_content($permalink);
 
         $user = new stdclass();
@@ -25,7 +44,7 @@ trait UpsertTrait {
             'siteId' => $this->site_id,
             'resource' => array(
                 'key' => $permalink,
-                'externalId' => $content->payload->guid,
+                'externalId' => $content->payload->guid . $langSuffix,
                 'type' => $content->payload->type,
                 'parentId' => strval($content->payload->parentId),
                 'order' => isset($content->payload->order) ? $content->payload->order : -1,
