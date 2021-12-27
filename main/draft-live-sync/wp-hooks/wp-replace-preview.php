4686d2ba91f7600d10275a178ab03f6317851aa8
Freddy Persson <freddy.persson@24hr.se>
2021-12-27 12:34:42 +0100

    Added the function of preview button leading to Draft again

--- main/draft-live-sync/wp-hooks/wp-replace-preview.php
+++ main/draft-live-sync/wp-hooks/wp-replace-preview.php
@@ -44,7 +44,7 @@ trait WpReplacePreview {
 
                 $jwt = JWT::encode($payload, $key);
 
-                $link = "$host/$slug?preview=$jwt";
+                $link = "$host/$slug";
 
                 header('Location: ' . $link);
             }
@@ -54,80 +54,40 @@ trait WpReplacePreview {
         add_action( 'admin_head', function () {
             global $post;
             echo "
-
                 <style>
                     button.block-editor-post-preview__button-toggle {
                         display: none;
                     }
                 </style>
-
                 <script type='text/javascript'>
-
                     window.addEventListener('load', () => {
+                        const originalButton = document.querySelector('[target=\"wp-preview-$post->ID\"]');
 
-                        let hasTakenOver = false;
+                        if (originalButton) {
+                            const previewLink = originalButton.href;
+                            const previewTarget = originalButton.target;
+                            const linkToPreview = document.createElement('a');
+                            linkToPreview.classList.add('components-button');
+                            linkToPreview.classList.add('is-secondary');
+                            linkToPreview.innerHTML = 'Preview';
+                            document.querySelector('.edit-post-header__settings').prepend(linkToPreview);
+                            linkToPreview.addEventListener('click', function(event) {
+                                if (wp.data.select('core/editor').isEditedPostDirty()) {
+                                    event.preventDefault();
+                                    event.stopPropagation();
+                                    wp.data
+                                        .dispatch('core/editor')
+                                        .savePost({ isPreview: true })
+                                        .then(() => {
+                                            window.open( previewLink, previewTarget );
+                                        });
+                                } else {
+                                    window.open( previewLink, previewTarget );
+                                }
 
-                        wp.data.subscribe(() => {
-                            // This is not the pretties way of checking if the preview functionality has been activated
-                            // but we havent found a good, documented, way to check if preview is enabled
-                            const originalButton = document.querySelector('[target=\"wp-preview-$post->ID\"]');
-                            if (originalButton) {
-                                previewTakeover();
-                            }
-                        });
-
-                        function previewTakeover() {
-
-                            if (hasTakenOver) {
-                                return;
-                            }
-
-                            const originalButton = document.querySelector('[target=\"wp-preview-$post->ID\"]');
-
-                            if (originalButton) {
-
-                                const previewTarget = originalButton.target;
-                                hasTakenOver = true;
-
-                                const linkToPreview = document.createElement('a');
-
-                                linkToPreview.classList.add('components-button');
-                                linkToPreview.classList.add('is-secondary');
-                                linkToPreview.innerHTML = 'Preview';
-
-                                document.querySelector('.edit-post-header__settings').prepend(linkToPreview);
-
-                                linkToPreview.addEventListener('click', function(event) {
-
-                                    if (wp.data.select('core/editor').isEditedPostDirty()) {
-
-                                        event.preventDefault();
-                                        event.stopPropagation();
-
-                                        wp.data
-                                            .dispatch('core/editor')
-                                            .autosave({ isPreview: true })
-                                            .then(() => {
-                                                const originalButton = document.querySelector('[target=\"wp-preview-$post->ID\"]');
-                                                const previewLink = originalButton.href;
-                                                window.open( previewLink, previewTarget );
-                                            });
-
-                                    } else {
-                                        const originalButton = document.querySelector('[target=\"wp-preview-$post->ID\"]');
-                                        const previewLink = originalButton.href;
-                                        window.open( previewLink, previewTarget );
-                                    }
-
-                                });
-
-                            }
-
+                            });
                         }
-
-                        previewTakeover();
-
-                   });
+                    });
                 </script>
             ";
         });
@@ -135,4 +95,3 @@ trait WpReplacePreview {
     }
 
 }
-
