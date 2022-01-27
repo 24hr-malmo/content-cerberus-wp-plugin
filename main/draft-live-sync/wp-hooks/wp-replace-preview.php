2e7cd03af8ad26dfabf02e679644fda29cb33ac5
Freddy Persson <freddy.persson@24hr.se>
2022-01-27 19:37:39 +0100

    Made preview button go to draft, also adapts to potential non-default language and if it's the start page

--- main/draft-live-sync/wp-hooks/wp-replace-preview.php
+++ main/draft-live-sync/wp-hooks/wp-replace-preview.php
@@ -1,27 +1,16 @@
 <?php
 
-use Firebase\JWT\JWT;
-
 trait WpReplacePreview {
 
     public function replace_preview() {
-
-        $key = getenv('PREVIEW_JWT_SECRET');
-        if (!isset($key)) {
-            add_action( 'admin_notices', function() {
-                $class = 'notice notice-error';
-                $message = __( 'PREVIEW_JWT_SECRET env not set. To make the preview functionality work, this env variable needs to be provided.', 'content-cerberus');
-                printf( '<div class="%1$s"><p>%2$s</p></div>', esc_attr( $class ), esc_html( $message ) ); 
-            });
-            array(&$this, 'show_site_id_missing_warning');
-        }
 
         add_action('template_redirect', function() {
 
             if ($_GET['preview'] == 'true' && $_GET['caller'] != 'content-service') {
 
                 global $post;
-                $key = getenv('PREVIEW_JWT_SECRET');
+                global $sitepress;
+                global $wp_query;
 
                 $domain_settings = $this->get_domain_settings(true, 'draft');
                 $host = get_site_url();
@@ -32,7 +21,26 @@ trait WpReplacePreview {
                     wp_die('Please set the correct domain in domain settings');
                 }
 
-                $slug =  $post->post_name;
+                $slug = $post->post_name;
+
+                $language = '';
+                if (isset($sitepress)) {
+                    $language = '/' . $sitepress->get_current_language();
+                    $default_language = '/' . $sitepress->get_default_language();
+
+                    //Remove default language, otherwise we get /${lang}/wp_block/ even when we go to default language page (which we normally treat as the base site)
+                    if ($language == $default_language) {
+                        $language = '';
+                    }
+                }
+
+                //If it's a start page we don't use the slug. Instead it will be at the / or ${lang}/
+                if (isset($wp_query)) {
+                    if ($wp_query->is_front_page()) {
+                        $slug = '';
+                    }
+                }
+
                 $iat = time();
                 $exp = $iat + (60 * 60 * 1); //will expire after 1 hour
                 $payload = array(
@@ -42,9 +50,7 @@ trait WpReplacePreview {
                     'cookies' => http_build_query($_COOKIE, '', ';'),
                 );
 
-                $jwt = JWT::encode($payload, $key);
-
-                $link = "$host/$slug?preview=$jwt";
+                $link = "$host$language/$slug";
 
                 header('Location: ' . $link);
             }
@@ -54,80 +60,40 @@ trait WpReplacePreview {
         add_action( 'admin_head', function () {
             global $post;
             echo "
-
                 <style>
                     button.block-editor-post-preview__button-toggle {
                         display: none;
                     }
                 </style>
-
                 <script type='text/javascript'>
-
                     window.addEventListener('load', () => {
+                        const originalButton = document.querySelector('[target=\"wp-preview-$post->ID\"]');
 
-                        let hasTakenOver = false;
+                        if (originalButton) {
+                            const previewLink = originalButton.href;
+                            const previewTarget = originalButton.target;
+                            const linkToPreview = document.createElement('a');
+                            linkToPreview.classList.add('components-button');
+                            linkToPreview.classList.add('is-secondary');
+                            linkToPreview.innerHTML = 'Preview';
+                            document.querySelector('.edit-post-header__settings').prepend(linkToPreview);
+                            linkToPreview.addEventListener('click', function(event) {
+                                if (wp.data.select('core/editor').isEditedPostDirty()) {
+                                    event.preventDefault();
+                                    event.stopPropagation();
+                                    wp.data
+                                        .dispatch('core/editor')
+                                        .savePost({ isPreview: true })
+                                        .then(() => {
+                                            window.open( previewLink, previewTarget );
+                                        });
+                                } else {
+                                    window.open( previewLink, previewTarget );
+                                }
 
-                        wp.data.subscribe(() => {
-                            // This is not the pretties way of checking if the preview functionality has been activated
-                            // but we havent found a good, documented, way to check if preview is enabled
-                            const originalButton = document.querySelector('[target=\"wp-preview-$post->ID\"]');
-                            if (originalButton) {
-                                previewTakeover();
-                            }
-                        });
-
-                        function previewTakeover() {
-
-                            if (hasTakenOver) {
-                                return;
-                            }
-
-                            const originalButton = document.querySelector('[target=\"wp-preview-$post->ID\"]');
-
-                            if (originalButton) {
-
-                                const previewTarget = originalButton.target;
-                                hasTakenOver = true;
-
-                                const linkToPreview = document.createElement('a');
-
-                                linkToPreview.classList.add('components-button');
-                                linkToPreview.classList.add('is-secondary');
-                                linkToPreview.innerHTML = 'Preview';
-
-                                document.querySelector('.edit-post-header__settings').prepend(linkToPreview);
-
-                                linkToPreview.addEventListener('click', function(event) {
-
-                                    if (wp.data.select('core/editor').isEditedPostDirty()) {
-
-                                        event.preventDefault();
-                                        event.stopPropagation();
-
-                                        wp.data
-                                            .dispatch('core/editor')
-                                            .autosave({ isPreview: true })
-                                            .then(() => {
-                                                const originalButton = document.querySelector('[target=\"wp-preview-$post->ID\"]');
-                                                const previewLink = originalButton.href;
-                                                window.open( previewLink, previewTarget );
-                                            });
-
-                                    } else {
-                                        const originalButton = document.querySelector('[target=\"wp-preview-$post->ID\"]');
-                                        const previewLink = originalButton.href;
-                                        window.open( previewLink, previewTarget );
-                                    }
-
-                                });
-
-                            }
-
+                            });
                         }
-
-                        previewTakeover();
-
-                   });
+                    });
                 </script>
             ";
         });
@@ -135,4 +101,3 @@ trait WpReplacePreview {
     }
 
 }
-
