4a3bed566fa25825532239459fb23f32cc9851e8
fp_turtle@hotmail.com <freddy.persson@24hr.se>
2022-04-04 11:10:30 +0200

    Bumped version and rebuild

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 Plugin Name: Cerberus Content Next
 Plugin URI: http://24hr.se
 Description: Saves content to a Draft Content Service and gives the possibility to push the content to live
-Version: 0.12.14
+Version: 0.12.19
 Author: Dr Tapia <camilo.tapia@24hr.se>
  */
 
