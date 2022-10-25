a777a7adab8097567c75f86922a20c24bdab8000
Peter Larsson <peter.larsson@24hr.se>
2022-10-25 11:02:53 +0200

    build version

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 Plugin Name: Cerberus Content Next
 Plugin URI: http://24hr.se
 Description: Saves content to a Draft Content Service and gives the possibility to push the content to live
-Version: 0.12.36
+Version: 0.12.38
 Author: Dr Tapia <camilo.tapia@24hr.se>
  */
 
