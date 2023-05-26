68b179b8c6fe3d8bd7006be37937166a7715270f
Christofer Haglund <christofer.haglund@24hr.se>
2023-05-26 13:54:27 +0200

    build version

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 Plugin Name: Cerberus Content Next
 Plugin URI: http://24hr.se
 Description: Saves content to a Draft Content Service and gives the possibility to push the content to live
-Version: 0.15.7-atos
+Version: 0.15.9-atos
 Author: Dr Tapia <camilo.tapia@24hr.se>
  */
 
