65b142151a264593b0d23772fa3c59608fe3f5bf
camilo <camilo.tapia@gmail.com>
2023-12-12 13:46:38 +0100

    build version

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '24hr/content-cerberus-wp-plugin',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '5aefb31f20120f524ed4e6b63d9d74b62f0a3584',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '24hr/content-cerberus-wp-plugin' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '5aefb31f20120f524ed4e6b63d9d74b62f0a3584',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
