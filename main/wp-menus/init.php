e215d42d5d0685b2870c0611d29b55a607b3e8fa
gimpan <jimmy.quach@twofour.se>
2026-02-24 14:04:32 +0100

    Refactor and remove debugging logging

--- main/wp-menus/init.php
+++ main/wp-menus/init.php
@@ -3,19 +3,14 @@
 include_once 'menu-generator.php';
 include_once 'api.php';
 
-
 /**
- * The current versions of WP and WPML, does not save header_menus language wise in the WP options table.
- * This callback generates our own option for location and store language-specific menu locations. A read callback also exists to retrieve our own options.
+ * Manually save locations using our own language aware option key. This is for saving in wordpress side.
+ * The current versions of WP and WPML, does not save locations language wise in the WP options table.
  * Normally you have only one wp option for locations. But we've made our custom locations entries that have a language suffix.
+ * Runs earlier than WPML callbacks. We could explore option variable and see if we can set our custom key there instead and pipe those values to next hook. So we do not need to run a double update_option.
  */
 add_filter('pre_update_option_theme_mods_rawb', function($value, $old_value, $option) {
     global $sitepress;
-
-    error_log('--- I AM AWAKE ---');
-    error_log('AWAKE1:' . var_export($value, true));
-    error_log('AWAKE1:' . var_export($old_value, true));
-    error_log('AWAKE1:' . var_export($option, true));
 
     if (!isset($sitepress) || !isset($value['nav_menu_locations'])) {
         return $value;
@@ -26,22 +21,19 @@ add_filter('pre_update_option_theme_mods_rawb', function($value, $old_value, $op
 
     // Store the ORIGINAL menu IDs before WPML converts them
     update_option($option_name, $value['nav_menu_locations']);
-    
 
     return $value; // Let WPML continue with its conversion
-}, 5, 3); // Run before WPML (priority 10)
+}, 5, 3);
 
 /**
+ * Manually get correct location based on language. This is when WP needs to return values to page.
  * The current versions of WP and WPML, does not save header_menus language wise in the WP options table.
- * This is a read callback that gives us our custom options.
- * We only wanted to retrieve this modified value for the menu admin page, so giving this last prio should not interfere with other callbacks.
  * Normally you have only one wp option for locations. But we've made our custom locations entries that have a language suffix.
+ * We only wanted to retrieve this modified value for the menu admin page, so giving this last prio should not interfere with other callbacks.
+ * But we could try running this much earlier, before WPML. It might help out with some issues and values we get.
  */
 add_filter('theme_mod_nav_menu_locations', function($locations) {
-    global $sitepress;
-
-    error_log('--- I AM ASLEEP ---');
-    error_log('ASLEEP1:' . var_export($locations, true));
+    global $sitepress;;
 
     if (!isset($sitepress)) {
         return $locations;
@@ -56,15 +48,12 @@ add_filter('theme_mod_nav_menu_locations', function($locations) {
     }
 
     return array();
-}, 999); // Run after WPML
-
-// WARNING! These action registrations are important and must be registered in the following order. DO NOT MOVE THEM.
+}, 999);
 
 /**
- * Upsert nav menu. This is draft
+ * Upsert nav menu. When menu is saved, we automatically sync to the draft version. Note this has nothing to do with locations, like header_menu.
  */
 add_action('wp_update_nav_menu', function ( $post_id, $data = NULL ) {
-    error_log('--- Updating nav menu - upserting menu to draft');
     global $draft_live_sync;
     global $sitepress;
 
@@ -80,21 +69,18 @@ add_action('wp_update_nav_menu', function ( $post_id, $data = NULL ) {
 }, 10, 3);
 
 /**
- * Sync changes to location header_menu only. Has a guard to prevent unnecessary operatoins, since the target hook runs on any theme change.
+ * Sync changes to location header_menu only. Has a guard to prevent unnecessary operations, since the target hook runs on any theme change.
  * Normally you have only one wp option for locations. But we've made our custom locations entries that have a language suffix.
- * This is draft.
+ * This is manages sync to draft.
  */
 add_action('update_option_theme_mods_rawb', function($old_value, $new_value) {
-
-    error_log('--- Updating nav locations - upserting menu location to draft');
-    error_log('--- DEBUG nav locations --- $old_value: ' . print_r($old_value, true));
-    error_log('--- DEBUG nav locations --- $new_value: ' . print_r($new_value, true));
 
     $locationKey = 'header_menu';
 
     // This can be risky, since this is the vanilla WP/WPML saving nav menus, which are not language aware.
     // Basically we rely on any save to locations with key header_menu, indicates we want to sync draft for that location.
     // Even though this old and new value, may not be for the correct language.
+    // Perhaps we can add a callback either on a earlier hook, or on this same hook, and tweak via a filter callback, with the correct value using language.
     if (($old_value['nav_menu_locations'][$locationKey] ?? null) === ($new_value['nav_menu_locations'][$locationKey] ?? null)) {
         return; // locations unchanged, skip
     }
@@ -113,37 +99,20 @@ add_action('update_option_theme_mods_rawb', function($old_value, $new_value) {
     $stored_locations = get_option($option_name, [])[$locationKey] ?? null; // Gives back an ID from WP
     $location_permalink = '/wp-json/content/v1/menus/' . $locationKey . $languageUrlSuffix;
 
-    error_log('--- DEBUG $option_name: ' . var_export($option_name, true));
-    error_log('--- DEBUG $stored_locations: ' . var_export($stored_locations, true));
-
     // Not null, we have a lang spec header menu in WP. Provide with permalink and upsert, it'll find it in WP.
     if ($stored_locations !== null) {
-        error_log('--- DEBUG $Not null, upsert to draft: ' . var_export($location_permalink, true));
         $draft_live_sync->upsert('draft', $location_permalink);
     }
     // Null, no lang spec header menu in WP anymore. Unpublish the permalink
     else if ($stored_locations === null) {
-        error_log('--- DEBUG $Is null, unpublish to draft: ' . var_export($location_permalink, true));
         $externalId = ContentCerberusMenuGenerator::build_external_id($locationKey, $current_lang);
         $draft_live_sync->unpublish('draft', $externalId, $location_permalink, );
     }
 }, 999, 2);
 
 /**
- * Sync location changes. Has a guard to prevent unnecessary operatoins, since the target hook runs on any theme change.
+ * Introduces a new option key in WP for storing our own langauge aware locations.
  */
-add_action('update_option_theme_mods_rawb', function($old_value, $new_value) {
-    // error_log('--- Updating nav locations - upserting menu to draft');
-
-    // error_log('--- Updating nav locations --- $old_value: ' . $old_value);
-
-    // error_log('--- Updating nav locations --- $new_value: ' . $new_value);
-    // if (($old_value['nav_menu_locations'] ?? []) === ($new_value['nav_menu_locations'] ?? [])) {
-    //     return; // locations unchanged, skip
-    // }
-    // We'll see if we need to do anything here
-}, 999, 2);
-
 add_action('init', function() {
 
     global $sitepress;
@@ -163,84 +132,6 @@ add_action('init', function() {
         }
 
     }
-
-    // add_action('wp_update_nav_menu', function ( $post_id, $data = NULL ) {
-        
-    //     error_log('--- Updating nav menu - upserting menu location to draft');
-    //     error_log('--- $data: ' . print_r($data, true));
-
-    //     global $draft_live_sync;
-    //     global $sitepress;
-
-    //     $language = '';
-    //     $languageSuffix = '';
-    //     if (isset($sitepress)) {
-    //         $language = '/' . $sitepress->get_current_language();
-    //         $languageSuffix = '-' . $sitepress->get_current_language();
-    //     }
-
-    //     $location_menus = get_nav_menu_locations(); // Fetches all menus with registered location
-    //     $menu_location = '';
-
-    //     foreach( $location_menus as $location => $menu_id ) {
-    //         if( $post_id == $menu_id ){
-    //             $menu_location = $location;
-    //         }
-    //     }
-
-    //     /**
-    //      * If the menu is associated with a location we must also add it to that location's entry in the database
-    //      * Location assignments ARE language-specific, so we include the language suffix
-    //      */
-
-    //     if ($menu_location !== '') {
-    //         $location_permalink = '/wp-json/content/v1/menus/' . $menu_location . $language;
-    //         $draft_live_sync->upsert('draft', $location_permalink);
-    //     }
-
-    // }, 10, 3);
-
-    // add_action('wp_update_nav_menu', function ( $post_id, $data = NULL ) {
-
-    //     error_log('--- Updating nav menu - Resync all menu location to draft');
-    //     error_log('--- $data: ' . print_r($data, true));
-
-    //     global $draft_live_sync;
-    //     global $sitepress;
-
-    //     $language = '';
-    //     $languageSuffix = '';
-    //     if (isset($sitepress)) {
-    //         $language = '/' . $sitepress->get_current_language();
-    //         $languageSuffix = '-' . $sitepress->get_current_language();
-    //     }
-
-    //     $location_menus = get_nav_menu_locations();
-
-    //      foreach( get_registered_nav_menus() as $registered_location => $name) { // Fetches all registered locations - even if they don't have an associated menu
-
-    //         if (!$location_menus[ $registered_location ]) {
-    //             /**
-    //              * If we find a registered_location without an associated menu, the content in the registered menu location entry in the DB should be empty.
-    //              * This may not be the case if the user has recently unset the location for a particular menu.
-    //              * So we loop through all registered menu locations and make sure that if WP doesn't have any content associated with it, our DB (draft) mirrors this state and
-    //              * we unpublish the menu from draft in case it's not already empty
-    //              */
-
-    //             $permalink_to_clear = '/wp-json/content/v1/menus/' . $registered_location . $language;
-    //             $externalId = 'menus-' . $registered_location . $languageSuffix;
-
-    //             $content = $draft_live_sync->get_content($permalink_to_clear);
-
-    //             if ($content->payload != '404' || !empty($content->payload)) {
-    //                 error_log('--- unpublishing deleted menu(s) from draft: ' . $permalink_to_clear);
-    //                 $draft_live_sync->unpublish('draft', $externalId, $permalink_to_clear);
-    //             }
-    //         }
-    //     }
-    // }, 10, 3);
-
-
 }, 100);
 
 
